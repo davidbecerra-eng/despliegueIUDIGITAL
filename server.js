2d340f78a5856643cbe9244dd79f02e5a2d65da7
davidbecerra-eng <david.becerra@est.iudigital.edu.co>
2026-03-25 13:32:35 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,31 +6,38 @@ const directoresRoutes = require("./routes/directores");
 const productorasRoutes = require("./routes/productoras");
 const tiposRoutes = require("./routes/tipos");
 const mediaRoutes = require("./routes/media");
+
 const app = express();
 app.use(cors());
-
 app.use(express.json());
+
 app.use("/generos", generosRoutes);
 app.use("/directores", directoresRoutes);
 app.use("/productoras", productorasRoutes);
 app.use("/tipos", tiposRoutes);
 app.use("/media", mediaRoutes);
+
 // Ruta de prueba
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
 });
 
-// Nueva ruta
+// Ruta de prueba DB
 app.get("/test-db", async (req, res) => {
   try {
     const [rows] = await pool.query("SELECT NOW() AS fecha");
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: "Error conectando a MySQL", detalle: error.message });
+    res.status(500).json({
+      error: "Error conectando a MySQL",
+      detalle: error.message,
+    });
   }
 });
 
 // Puerto del servidor
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
